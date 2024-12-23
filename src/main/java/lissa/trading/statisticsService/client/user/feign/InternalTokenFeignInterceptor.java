c1eb9870d115b4f97519b4caa8e19d08a3573b28
SashaTyutyaev <sashatut2002@yandex.ru>
2024-12-23 20:42:42 +0300

    remove word bearer

--- src/main/java/lissa/trading/statisticsService/client/user/feign/InternalTokenFeignInterceptor.java
+++ src/main/java/lissa/trading/statisticsService/client/user/feign/InternalTokenFeignInterceptor.java
@@ -16,7 +16,7 @@ public class InternalTokenFeignInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        requestTemplate.header("Authorization", "Bearer " +
+        requestTemplate.header("Authorization",
                 new String(Base64.getDecoder().decode(internalToken), StandardCharsets.UTF_8).trim());
     }
 }
