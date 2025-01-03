c699dfaa15cff497a62444e4bfe0ffbb19c5c92c
SashaTyutyaev <sashatut2002@yandex.ru>
2025-01-04 00:33:06 +0300

    feat(STOCK-21) added random data to database

---
+++ src/main/java/lissa/trading/statisticsService/service/dataInitializer/MainDataInitializer.java
@@ -0,0 +1,20 @@
+package lissa.trading.statisticsService.service.dataInitializer;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MainDataInitializer {
+    private final List<DataInitializerService> dataInitializerServices;
+
+    @PostConstruct
+    public void init() {
+        dataInitializerServices.forEach(DataInitializerService::createData);
+    }
+}
