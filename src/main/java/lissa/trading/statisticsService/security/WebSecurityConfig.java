74938c63ee461ea4130954aa1493c4cbf3b3f8b5
blaskowitz <blaskowitzwork@gmail.com>
2025-02-11 14:42:12 +0300

    feat(STOCK-39) - configured monitoring with spring boot actuator

--- src/main/java/lissa/trading/statisticsService/security/WebSecurityConfig.java
+++ src/main/java/lissa/trading/statisticsService/security/WebSecurityConfig.java
@@ -3,14 +3,31 @@ package lissa.trading.statisticsService.security;
 import lissa.trading.lissa.auth.lib.dto.UserInfoDto;
 import lissa.trading.lissa.auth.lib.security.BaseAuthTokenFilter;
 import lissa.trading.lissa.auth.lib.security.BaseWebSecurityConfig;
+import lissa.trading.statisticsService.security.internal.InternalTokenFilter;
+import lissa.trading.statisticsService.security.internal.InternalTokenService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig extends BaseWebSecurityConfig {
+    private final InternalTokenService internalTokenService;
 
-    public WebSecurityConfig(BaseAuthTokenFilter<UserInfoDto> authTokenFilter) {
+    public WebSecurityConfig(BaseAuthTokenFilter<UserInfoDto> authTokenFilter, InternalTokenService internalTokenService) {
         super(authTokenFilter);
+        this.internalTokenService = internalTokenService;
+    }
+
+    @Bean
+    public InternalTokenFilter internalTokenFilter() {
+        return new InternalTokenFilter(internalTokenService);
+    }
+
+    @Override
+    protected void configureHttpSecurity(HttpSecurity http) throws Exception {
+        http.addFilterBefore(internalTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
